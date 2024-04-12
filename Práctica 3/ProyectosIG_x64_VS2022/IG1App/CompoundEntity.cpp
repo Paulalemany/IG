91805661e740bc00c17999d07cc2096657b4ae3d
POKOLI0 <psierr01@ucm.es>
2024-04-12 16:15:50 +0200

    intento 59-60

---
+++ Práctica 3/ProyectosIG_x64_VS2022/IG1App/CompoundEntity.cpp
@@ -0,0 +1,9 @@
+#include "CompoundEntity.h"
+
+void CompoundEntity::addEntity(Abs_Entity* ae)
+{
+}
+
+void CompoundEntity::render(glm::dmat4 const& modelViewMat) const
+{
+}
---
+++ Práctica 3/ProyectosIG_x64_VS2022/IG1App/CompoundEntity.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Entity.h"
+class CompoundEntity :
+    public Abs_Entity
+{
+public:
+    //APT59
+    CompoundEntity() {};
+    ~CompoundEntity() {};
+
+    void addEntity(Abs_Entity* ae);
+    void render(glm::dmat4 const& modelViewMat) const override;
+
+private:
+    std::vector<Abs_Entity*> gObjects;
+
+
+};
+
+class AdvancedTIE : public CompoundEntity
+{
+public:
+    Sphere(GLdouble r);
+    void render(glm::dmat4 const& modelViewMat) const;
+
+protected:
+    GLdouble radius;
+
+};
+
