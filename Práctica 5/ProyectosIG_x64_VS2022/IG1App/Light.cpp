476c344caae68ebf2768ab1b85f2af2b9452fece
Paulalemany <palemany@ucm.es>
2024-05-10 18:00:44 +0200

    proceso clase light

---
+++ Práctica 5/ProyectosIG_x64_VS2022/IG1App/Light.cpp
@@ -0,0 +1,19 @@
+#include "Light.h"
+
+Light::Light()
+{
+	if (cont < GL_MAX_LIGHTS) {
+		id = GL_LIGHT0 + cont;
+		++cont;
+		glEnable(id);
+	}
+}
+
+	void Light::uploadL()
+	{
+		// Transfiere las características de la luz a la GPU
+		glLightfv(id, GL_AMBIENT, value_ptr(ambient));
+		glLightfv(id, GL_DIFFUSE, value_ptr(diffuse));
+		glLightfv(id, GL_SPECULAR, value_ptr(specular));
+
+	}
